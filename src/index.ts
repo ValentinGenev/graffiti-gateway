5f3e4ffc5485b54a672ccc4c1c2a4d3db839efec
valentingenev <valentin_genev@abv.bg>
2022-01-29 16:32:00 +0200

    [=] the gateway is up and working in the same network;

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+import * as _rest from './lib/rest'
+
+dotenv.config()
+
+export const rest = new _rest.RestServer({
+    port: process.env.REST_PORT
+})
+
+if (process.env.NODE_ENV !== 'test') {
+    _rest.useBodyParser(rest)
+    _rest.start(rest)
+}
