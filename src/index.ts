5bca4e59a2e7d4713318949df9da2ce852ae7a95
valentingenev <valentin_genev@abv.bg>
2022-02-25 22:35:07 +0200

    [+] response headers; [+] CORS rules; [+] client env variables;

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import dotenv from 'dotenv'
+import { Header } from './interfaces/IServer'
 import * as _rest from './lib/rest'
 import * as _messagesRouter from './controllers/messages'
 
 dotenv.config()
 
-export const rest = new _rest.RestServer({
-    port: process.env.REST_PORT
+const {
+    REST_PORT,
+    CLIENT_PROTOCOL,
+    CLIENT_HOST,
+    CLIENT_PORT,
+    NODE_ENV
+} = process.env
+const restHeaders: Header[] = [
+    {
+        key: 'Access-Control-Allow-Origin',
+        value: `${CLIENT_PROTOCOL}://${CLIENT_HOST}:${CLIENT_PORT}`
+    }
+]
+const rest = new _rest.RestServer({
+    port: REST_PORT
 })
 
-if (process.env.NODE_ENV !== 'test') {
+if (NODE_ENV !== 'test') {
     _rest.useBodyParser(rest)
     _rest.start(rest)
     _rest.useFingerPrint(rest)
+    _rest.setResponseHeaders(rest, restHeaders)
     _messagesRouter.setMessagesRoutes(rest)
 }
--- src/lib/rest.ts
+++ src/lib/rest.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import fingerprint from 'express-fingerprint'
+import { Header } from '../interfaces/IServer'
 
 interface ServerConfiguration {
     port?: string
@@ -29,6 +30,19 @@ export function useBodyParser(rest: RestServer): void {
 
 export function useFingerPrint(rest: RestServer): void {
     rest.getServer().use(fingerprint())
+}
+
+export function setResponseHeaders(
+    rest: RestServer,
+    headers: Header[]
+): void {
+    rest.getServer().use((_request, response, next) => {
+        for (const header of headers) {
+            response.append(header.key, header.value)
+        }
+
+        next()
+    })
 }
 
 export function start(rest: RestServer): void {
